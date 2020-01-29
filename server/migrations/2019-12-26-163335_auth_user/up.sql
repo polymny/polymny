eedd4768c7f017f8589d135b328144380d9b55f8
Nicolas Bertrand <nico@inattendu.org>
2020-01-29 15:25:51 +0100

    Do diesel updates on the same file

--- server/migrations/2019-12-26-163335_auth_user/up.sql
+++ server/migrations/2019-12-26-163335_auth_user/up.sql
@@ -15,3 +15,22 @@ CREATE TABLE sessions (
     secret VARCHAR NOT NULL
 );
 
+CREATE TABLE projects (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users (id),
+    projectname VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE capsules (
+    id SERIAL PRIMARY KEY,
+    name  VARCHAR NOT NULL UNIQUE,
+    title VARCHAR,
+    slides VARCHAR,
+    description TEXT
+);
+
+CREATE TABLE capsules_projects (
+  id SERIAL PRIMARY KEY,
+  capsule_id SERIAL references capsules(id),
+  project_id SERIAL references projects(id)
+)
