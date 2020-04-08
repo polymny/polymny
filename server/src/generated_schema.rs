ac49167568aa71ab1bcb0c0433e8ff05de156b00
Thomas Forgione <thomas@forgione.fr>
2020-04-08 12:46:33 +0200

    Update

--- server/src/generated_schema.rs
+++ server/src/generated_schema.rs
@@ -28,6 +28,14 @@ table! {
 }
 
 table! {
+    goss (id) {
+        id -> Int4,
+        position -> Int4,
+        capsule_id -> Int4,
+    }
+}
+
+table! {
     projects (id) {
         id -> Int4,
         user_id -> Int4,
@@ -78,6 +86,7 @@ allow_tables_to_appear_in_same_query!(
     assets,
     capsules,
     capsules_projects,
+    goss,
     projects,
     sessions,
     slides,
