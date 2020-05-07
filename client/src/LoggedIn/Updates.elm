085f532dc95614a964de703db448122281e44f96
Thomas Forgione <thomas@forgione.fr>
2020-05-07 10:00:33 +0200

    Fix project clicked

--- client/src/LoggedIn/Updates.elm
+++ client/src/LoggedIn/Updates.elm
@@ -1,9 +1,11 @@
 module LoggedIn.Updates exposing (update)
 
+import Api
 import Core.Types as Core
 import LoggedIn.Types as LoggedIn
 import Preparation.Types as Preparation
 import Preparation.Updates as Preparation
+import Utils
 
 
 update : LoggedIn.Msg -> LoggedIn.Model -> ( LoggedIn.Model, Cmd Core.Msg )
@@ -16,5 +18,22 @@ update msg { session, tab } =
             in
             ( LoggedIn.Model session (LoggedIn.Preparation newModel), cmd )
 
+        ( LoggedIn.PreparationMsg (Preparation.ProjectClicked project), _ ) ->
+            ( { session = session, tab = LoggedIn.Preparation (Preparation.Model session (Preparation.Project project)) }
+            , Api.capsulesFromProjectId (resultToMsg project) project.id
+            )
+
         _ ->
             ( LoggedIn.Model session tab, Cmd.none )
+
+
+resultToMsg : Api.Project -> Result e (List Api.Capsule) -> Core.Msg
+resultToMsg project result =
+    Utils.resultToMsg
+        (\x ->
+            Core.LoggedInMsg <|
+                LoggedIn.PreparationMsg <|
+                    Preparation.CapsulesReceived project x
+        )
+        (\_ -> Core.Noop)
+        result
