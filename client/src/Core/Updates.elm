d960e81cb7472e4b6bdaaf9ed0378a831b458806
Thomas Forgione <thomas@forgione.fr>
2020-05-30 14:54:30 +0200

    Turn off webcam when exiting

--- client/src/Core/Updates.elm
+++ client/src/Core/Updates.elm
@@ -1,5 +1,6 @@
 module Core.Updates exposing (update)
 
+import Acquisition.Ports as Ports
 import Core.Types as Core
 import LoggedIn.Types as LoggedIn
 import LoggedIn.Updates as LoggedIn
@@ -14,67 +15,95 @@ import SignUp.Updates as SignUp
 
 update : Core.Msg -> Core.FullModel -> ( Core.FullModel, Cmd Core.Msg )
 update msg { global, model } =
-    case ( msg, model ) of
-        -- INNER MESSAGES
-        ( Core.Noop, _ ) ->
-            ( Core.FullModel global model, Cmd.none )
+    let
+        ( returnModel, returnCmd ) =
+            case ( msg, model ) of
+                -- INNER MESSAGES
+                ( Core.Noop, _ ) ->
+                    ( Core.FullModel global model, Cmd.none )
 
-        ( Core.TimeZoneChanged newTimeZone, _ ) ->
-            ( Core.FullModel { global | zone = newTimeZone } model, Cmd.none )
+                ( Core.TimeZoneChanged newTimeZone, _ ) ->
+                    ( Core.FullModel { global | zone = newTimeZone } model, Cmd.none )
 
-        ( Core.HomeClicked, Core.LoggedIn { session } ) ->
-            ( Core.FullModel global
-                (Core.LoggedIn
-                    { session = session
-                    , tab =
-                        LoggedIn.Preparation Preparation.Home
-                    }
-                )
-            , Cmd.none
-            )
+                ( Core.HomeClicked, Core.LoggedIn { session } ) ->
+                    ( Core.FullModel global
+                        (Core.LoggedIn
+                            { session = session
+                            , tab =
+                                LoggedIn.Preparation Preparation.Home
+                            }
+                        )
+                    , Cmd.none
+                    )
 
-        ( Core.LoginClicked, _ ) ->
-            ( Core.FullModel global (Core.Login Login.init), Cmd.none )
+                ( Core.LoginClicked, _ ) ->
+                    ( Core.FullModel global (Core.Login Login.init), Cmd.none )
 
-        ( Core.LogoutClicked, _ ) ->
-            ( Core.FullModel global Core.Home, Cmd.none )
+                ( Core.LogoutClicked, _ ) ->
+                    ( Core.FullModel global Core.Home, Cmd.none )
 
-        ( Core.SignUpClicked, _ ) ->
-            ( Core.FullModel global (Core.SignUp SignUp.init), Cmd.none )
+                ( Core.SignUpClicked, _ ) ->
+                    ( Core.FullModel global (Core.SignUp SignUp.init), Cmd.none )
 
-        ( Core.NewProjectClicked, Core.LoggedIn { session } ) ->
-            ( Core.FullModel global
-                (Core.LoggedIn
-                    { session = session
-                    , tab =
-                        LoggedIn.Preparation <|
-                            Preparation.NewProject NewProject.init
-                    }
-                )
-            , Cmd.none
-            )
+                ( Core.NewProjectClicked, Core.LoggedIn { session } ) ->
+                    ( Core.FullModel global
+                        (Core.LoggedIn
+                            { session = session
+                            , tab =
+                                LoggedIn.Preparation <|
+                                    Preparation.NewProject NewProject.init
+                            }
+                        )
+                    , Cmd.none
+                    )
 
-        -- OTHER MODULES MESSAGES
-        ( Core.LoginMsg loginMsg, Core.Login loginModel ) ->
-            let
-                ( m, cmd ) =
-                    Login.update loginMsg loginModel
-            in
-            ( Core.FullModel global m, cmd )
+                -- OTHER MODULES MESSAGES
+                ( Core.LoginMsg loginMsg, Core.Login loginModel ) ->
+                    let
+                        ( m, cmd ) =
+                            Login.update loginMsg loginModel
+                    in
+                    ( Core.FullModel global m, cmd )
 
-        ( Core.SignUpMsg signUpMsg, Core.SignUp signUpModel ) ->
-            let
-                ( m, cmd ) =
-                    SignUp.update signUpMsg signUpModel
-            in
-            ( Core.FullModel global (Core.SignUp m), cmd )
+                ( Core.SignUpMsg signUpMsg, Core.SignUp signUpModel ) ->
+                    let
+                        ( m, cmd ) =
+                            SignUp.update signUpMsg signUpModel
+                    in
+                    ( Core.FullModel global (Core.SignUp m), cmd )
 
-        ( Core.LoggedInMsg newProjectMsg, Core.LoggedIn { session, tab } ) ->
-            let
-                ( m, cmd ) =
-                    LoggedIn.update newProjectMsg (LoggedIn.Model session tab)
-            in
-            ( Core.FullModel global (Core.LoggedIn m), cmd )
+                ( Core.LoggedInMsg newProjectMsg, Core.LoggedIn { session, tab } ) ->
+                    let
+                        ( m, cmd ) =
+                            LoggedIn.update newProjectMsg (LoggedIn.Model session tab)
+                    in
+                    ( Core.FullModel global (Core.LoggedIn m), cmd )
+
+                _ ->
+                    ( Core.FullModel global model, Cmd.none )
+
+        -- If model is acquisition and returnModel is not, we need to turn off the camera
+        closeCamera =
+            case ( isAcquisition model, isAcquisition returnModel.model ) of
+                ( True, False ) ->
+                    Ports.exit ()
+
+                _ ->
+                    Cmd.none
+    in
+    ( returnModel, Cmd.batch [ returnCmd, closeCamera ] )
+
+
+isAcquisition : Core.Model -> Bool
+isAcquisition model =
+    case model of
+        Core.LoggedIn { tab } ->
+            case tab of
+                LoggedIn.Acquisition _ ->
+                    True
+
+                _ ->
+                    False
 
         _ ->
-            ( Core.FullModel global model, Cmd.none )
+            False
