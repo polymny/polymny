325f7b626cce5a8dd3dedceed63957546561dd69
Thomas Forgione <thomas@forgione.fr>
2020-06-02 16:43:21 +0200

    Removed useless func

--- client/src/Log.elm
+++ client/src/Log.elm
@@ -1,6 +1,4 @@
-module Log exposing (debug, debugSlides)
-
-import Capsule.Types as Capsule
+module Log exposing (debug)
 
 
 debug : String -> a -> a
@@ -13,23 +11,3 @@ debug message value =
 -- debug : String -> a -> a
 -- debug message value =
 --     value
-
-
-debugSlides : String -> List Capsule.MaybeSlide -> List Capsule.MaybeSlide
-debugSlides message slides =
-    let
-        _ =
-            debug message
-                (List.map
-                    (\x ->
-                        case x of
-                            Capsule.GosId id ->
-                                "GosId " ++ String.fromInt id
-
-                            Capsule.JustSlide s _ ->
-                                "JustSlide " ++ s.prompt
-                    )
-                    slides
-                )
-    in
-    slides
