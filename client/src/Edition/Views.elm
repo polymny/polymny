d87c0a412b0705c56e9e9774df56417c60c99168
Thomas Forgione <thomas@forgione.fr>
2020-09-23 15:51:04 +0200

    Keep cleaning

--- client/src/Edition/Views.elm
+++ client/src/Edition/Views.elm
@@ -4,37 +4,15 @@ import Api
 import Core.Types as Core
 import Edition.Types as Edition
 import Element exposing (Element)
-import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
-import Html exposing (Html)
-import Html.Attributes
 import LoggedIn.Types as LoggedIn
 import Preparation.Views as Preparation
 import Status
-import Ui.Attributes as Attributes
 import Ui.Colors as Colors
 import Ui.Ui as Ui
 import Webcam
-
-
-
---view : Core.Global -> Api.Session -> Edition.Model -> Element Core.Msg
---view global _ model =
---    let
---        mainPage =
---            mainView global model
---
---        element =
---            Element.column Ui.mainViewAttributes2
---                [ Utils.headerView "edition" model.details
---                , mainPage
---                ]
---    in
---    Element.row Ui.mainViewAttributes1
---
---[ element ]
 
 
 view : Core.Global -> Api.Session -> Edition.Model -> Element Core.Msg
@@ -52,8 +30,7 @@ centerView global model =
             List.head (List.drop model.currentGos model.details.structure)
     in
     Element.column [ Element.width (Element.fillPortion 7), Element.height Element.fill ]
-        [ -- capsuleProductionView global model
-          gosProductionView model gos
+        [ gosProductionView model gos
         , bottomRow global model
         ]
 
@@ -63,7 +40,7 @@ gosProductionView model gos =
     let
         resultView =
             case gos of
-                Just g ->
+                Just _ ->
                     Element.row
                         [ Element.width Element.fill
                         , Element.height Element.fill
@@ -76,105 +53,6 @@ gosProductionView model gos =
                     Element.none
     in
     resultView
-
-
-slidesView : List Api.Slide -> Element Core.Msg
-slidesView slides =
-    Element.row []
-        (List.map (\x -> Element.el [ Element.padding 2 ] <| Element.text <| String.fromInt x.id) slides)
-
-
-capsuleProductionView : Core.Global -> Edition.Model -> Element Core.Msg
-capsuleProductionView global model =
-    let
-        details =
-            model.details
-
-        status =
-            model.status
-
-        video =
-            case details.video of
-                Just x ->
-                    Element.el
-                        [ Border.color Colors.artEvening
-                        , Border.rounded 0
-                        , Border.width 2
-                        , Element.padding 2
-                        , Element.centerX
-                        , Element.centerY
-                        ]
-                    <|
-                        Element.html <|
-                            htmlVideo x.asset_path
-
-                Nothing ->
-                    Element.el [] <| Element.text "Pas de vidéo éditée pour l'instant"
-
-        url_video : Api.Asset -> String
-        url_video asset =
-            global.videoRoot ++ "/?v=" ++ asset.uuid ++ "/"
-
-        button =
-            case ( details.capsule.published, details.video ) of
-                ( Api.NotPublished, Just _ ) ->
-                    Ui.primaryButton (Just Edition.PublishVideo) "Publier la vidéo"
-                        |> Element.map LoggedIn.EditionMsg
-                        |> Element.map Core.LoggedInMsg
-
-                ( Api.Publishing, _ ) ->
-                    Ui.messageWithSpinner "Publication de vidéo en cours..."
-
-                ( Api.Published, Just v ) ->
-                    Element.column
-                        (Attributes.boxAttributes
-                            ++ [ Element.spacing 20 ]
-                        )
-                        [ Element.newTabLink
-                            [ Element.centerX
-                            ]
-                            { url = url_video v
-                            , label = Ui.primaryButton Nothing "Voir la vidéo publiée"
-                            }
-                        , Element.text "Lien vers la vidéo publiée : "
-                        , Element.el
-                            [ Background.color Colors.white
-                            , Border.color Colors.whiteDarker
-                            , Border.rounded 5
-                            , Border.width 1
-                            , Element.paddingXY 10 10
-                            , Attributes.fontMono
-                            ]
-                          <|
-                            Element.text <|
-                                url_video v
-                        ]
-
-                ( _, _ ) ->
-                    Element.none
-
-        ( element, publishButton ) =
-            case status of
-                Status.Sent ->
-                    ( Ui.messageWithSpinner "Edition automatique en cours", Element.none )
-
-                Status.Success () ->
-                    ( video, button )
-
-                Status.Error () ->
-                    ( Element.text "Problème rencontré lors de la compostion de la vidéo. Merci de nous contacter", Element.none )
-
-                Status.NotSent ->
-                    ( video, button )
-    in
-    Element.row []
-        [ editionOptionView model
-        , Element.column
-            [ Element.centerX, Element.spacing 20, Element.padding 10 ]
-            [ element
-            , publishButton
-            ]
-        ]
 
 
 gosProductionChoicesView : Edition.Model -> Element Core.Msg
@@ -283,107 +161,6 @@ gosProductionChoicesView model =
     Element.map Core.LoggedInMsg <|
         Element.map LoggedIn.EditionMsg <|
             Element.column [ Element.alignLeft, Element.padding 10, Element.spacing 30 ]
-                form
-
-
-editionOptionView : Edition.Model -> Element Core.Msg
-editionOptionView { status, withVideo, webcamSize, webcamPosition } =
-    let
-        submitOnEnter =
-            case status of
-                Status.Sent ->
-                    []
-
-                Status.Success () ->
-                    []
-
-                _ ->
-                    [ Ui.onEnter Edition.OptionsSubmitted ]
-
-        submitButton =
-            case status of
-                Status.Sent ->
-                    Ui.primaryButtonDisabled "en cours ...."
-
-                _ ->
-                    Element.el [ Font.center ] <|
-                        Ui.primaryButton
-                            (Just Edition.OptionsSubmitted)
-                            "Valider les options et génerer \n la vidéo de la capsule"
-
-        videoFields =
-            [ Input.radio
-                [ Element.padding 10
-                , Element.spacing 10
-                ]
-                { onChange = Edition.WebcamSizeChanged
-                , selected = Just webcamSize
-                , label =
-                    Input.labelAbove
-                        [ Element.centerX
-                        , Font.bold
-                        , Element.padding 1
-                        ]
-                        (Element.text "Taille de l'incrustation webcam:")
-                , options =
-                    [ Input.option Webcam.Small (Element.text "Petit")
-                    , Input.option Webcam.Medium (Element.text "Moyen")
-                    , Input.option Webcam.Large (Element.text "Grand")
-                    ]
-                }
-            , Input.radio
-                [ Element.padding 10
-                , Element.spacing 10
-                ]
-                { onChange = Edition.WebcamPositionChanged
-                , selected = Just webcamPosition
-                , label =
-                    Input.labelAbove
-                        [ Element.centerX
-                        , Font.bold
-                        , Element.padding 1
-                        ]
-                        (Element.text "Position de l'incrustation:")
-                , options =
-                    [ Input.option Webcam.TopLeft (Element.text "En haut à gauche.")
-                    , Input.option Webcam.TopRight (Element.text "En haut à droite.")
-                    , Input.option Webcam.BottomLeft (Element.text "En bas à gauche.")
-                    , Input.option Webcam.BottomRight (Element.text "En bas à droite.")
-                    ]
-                }
-            ]
-
-        commmonFields =
-            Input.checkbox []
-                { onChange = Edition.WithVideoChanged
-                , icon = Input.defaultCheckbox
-                , checked = withVideo
-                , label =
-                    Input.labelRight [] <|
-                        Element.text <|
-                            if withVideo then
-                                "L'audio et la vidéo seront utilisés"
-
-                            else
-                                "Seul l'audio sera utilisé"
-                }
-
-        fields =
-            if withVideo then
-                (commmonFields :: videoFields) ++ [ submitButton ]
-
-            else
-                [ commmonFields, submitButton ]
-
-        header =
-            Element.row [ Element.centerX, Font.bold ] [ Element.text "Options d'édition de la vidéo" ]
-
-        form =
-            header :: fields
-    in
-    Element.map Core.LoggedInMsg <|
-        Element.map LoggedIn.EditionMsg <|
-            Element.column [ Element.centerX, Element.padding 10, Element.spacing 30 ]
                 form
 
 
@@ -535,44 +312,9 @@ bottomRow global model =
                         , Ui.chainButton (Just (Core.LoggedInMsg (LoggedIn.EditionMsg (Edition.CopyUrl (videoUrl v))))) "" "Copier l'url de la vidéo"
                         ]
 
-                -- Element.column
-                --     (Attributes.boxAttributes
-                --         ++ [ Element.spacing 20 ]
-                --     )
-                --     [ Element.newTabLink
-                --         [ Element.centerX
-                --         ]
-                --         { url = videoUrl v
-                --         , label = Ui.primaryButton Nothing "Voir la vidéo publiée"
-                --         }
-                --     , Element.text "Lien vers la vidéo publiée : "
-                --     , Element.el
-                --         [ Background.color Colors.white
-                --         , Border.color Colors.whiteDarker
-                --         , Border.rounded 5
-                --         , Border.width 1
-                --         , Element.paddingXY 10 10
-                --         , Attributes.fontMono
-                --         ]
-                --       <|
-                --         Element.text <|
-                --             videoUrl v
-                --     ]
                 ( _, _ ) ->
                     Element.none
     in
     Element.row
         [ Element.alignRight, Element.padding 10, Element.spacing 10 ]
         [ element, editButton, publishButton ]
-
-
-htmlVideo : String -> Html msg
-htmlVideo url =
-    Html.video
-        [ Html.Attributes.controls True
-        , Html.Attributes.width 600
-        ]
-        [ Html.source
-            [ Html.Attributes.src url ]
-            []
-        ]
