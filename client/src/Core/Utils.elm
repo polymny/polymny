39a3add36433ee16671febf79d4e9aefd11139ea
Thomas Forgione <thomas@forgione.fr>
2020-10-20 14:36:11 +0200

    Remove testnotif

--- client/src/Core/Utils.elm
+++ client/src/Core/Utils.elm
@@ -126,21 +126,9 @@ globalFromFlags flags key =
     , numberOfSlidesPerRow = 3
     , expiry = 0
     , showAbout = False
-    , notifications = testNotif
+    , notifications = []
     , notificationPanelVisible = False
     }
-
-
-testNotif : List Core.Notification
-testNotif =
-    [ Core.notification "Bonjour" "Bonjour Toto"
-    , { title = "Bonjour", content = "Bonjour Tata", read = True }
-    , Core.notification "Bonjour" "Bonjour Toto"
-    , { title = "Bonjour", content = "Bonjour Tata", read = True }
-    , Core.notification "Bonjour" "Bonjour Toto"
-    , Core.notification "Bonjour" "Bonjour Toto"
-    , { title = "Bonjour", content = "Bonjour Tata", read = True }
-    ]
 
 
 modelFromFlags : Core.Global -> Decode.Value -> ( Core.Model, Cmd Core.Msg )
