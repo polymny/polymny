73254acdf1273527599bfdca125c0b368dfd9528
Thomas Forgione <thomas@forgione.fr>
2020-10-29 14:52:12 +0100

    Basic username / email verification

--- client/src/SignUp/Updates.elm
+++ client/src/SignUp/Updates.elm
@@ -2,6 +2,7 @@ module SignUp.Updates exposing (update)
 
 import Api
 import Core.Types as Core
+import Http
 import SignUp.Types as SignUp
 import Status
 
@@ -23,8 +24,23 @@ update msg content =
 
         SignUp.Submitted ->
             ( { content | status = Status.Sent }
-            , Api.signUp (\_ -> Core.SignUpMsg SignUp.Success) content
+            , Api.signUp resultToMsg content
             )
 
         SignUp.Success ->
             ( { content | status = Status.Success () }, Cmd.none )
+
+        SignUp.Failed message ->
+            ( { content | status = Status.Error message }, Cmd.none )
+
+
+resultToMsg result =
+    case result of
+        Ok _ ->
+            Core.SignUpMsg SignUp.Success
+
+        Err (Http.BadStatus 404) ->
+            Core.SignUpMsg (SignUp.Failed "Le nom d'utilisateur ou l'addresse e-mail est déjà utilisée")
+
+        Err _ ->
+            Core.SignUpMsg (SignUp.Failed "L'inscription a échouée pour une raison inconnue")
