04f722e405dadb92d2ed7ae5fa93a4d77a697317
Nicolas Bertrand <nico@inattendu.org>
2020-11-11 23:06:08 +0100

    Update on mobile view

--- client/src/Core/Views.elm
+++ client/src/Core/Views.elm
@@ -105,7 +105,7 @@ viewContent { global, model } =
         ( content, givenPopup ) =
             case model of
                 Core.Home homeModel ->
-                    ( homeView homeModel, Nothing )
+                    ( homeView homeModel global.device, Nothing )
 
                 Core.ResetPassword resetPasswordModel ->
                     ( ResetPassword.view resetPasswordModel, Nothing )
@@ -155,8 +155,8 @@ viewContent { global, model } =
         ]
 
 
-homeView : Core.HomeModel -> Element Core.Msg
-homeView model =
+homeView : Core.HomeModel -> Element.Device -> Element Core.Msg
+homeView model device =
     let
         forgotPasswordLinkContent =
             Ui.linkButton (Just Core.ForgotPasswordClicked) "Mot de passe oublié"
@@ -232,7 +232,7 @@ homeView model =
                     Element.text "Tutoriels d'utilisation de polymny "
             }
         , partnersView
-        , featuresView
+        , featuresView device
         , tutosView
         , Element.textColumn [ Element.centerX, Element.width <| Element.maximum 1000 Element.fill, Font.size 20, Element.padding 30, Element.spacing 20 ]
             [ Element.paragraph []
@@ -507,32 +507,51 @@ bottomBar global =
         ]
 
 
-featuresView : Element Core.Msg
-featuresView =
-    Element.column [ Element.centerX, Element.width Element.fill, Element.spacing 20, Font.justify, Font.size 20 ]
+featuresView : Element.Device -> Element Core.Msg
+featuresView device =
+    let
+        imageSize =
+            500
+
+        twoColMaximum =
+            500
+
+        ( builder, contentPadding ) =
+            case device.class of
+                Element.Phone ->
+                    ( Element.column [ Element.width Element.fill, Element.height Element.fill, Element.spacing 10 ]
+                    , Element.paddingXY 10 0
+                    )
+
+                _ ->
+                    ( Element.row [ Element.width Element.fill, Element.padding 80, Element.spacing 10 ]
+                    , Element.padding 0
+                    )
+    in
+    Element.column [ Element.centerX, Element.width Element.fill, contentPadding, Font.justify, Font.size 20 ]
         [ Assets.videoBonjour
-        , Element.row [ Element.width Element.fill, Element.height Element.fill, Element.padding 50, Element.spacing 20 ]
+        , builder
             [ Element.column
                 [ Element.width Element.fill
                 , Element.centerY
                 , Element.spacing 10
                 ]
                 [ Element.el [ Element.centerX, Border.rounded 100 ] <|
-                    Element.image [ Element.width <| Element.px 700 ]
+                    Element.image [ Element.width <| Element.px imageSize ]
                         { src = "/dist/moodle.png"
                         , description = "Partager des capsules sur moodle"
                         }
                 ]
-            , Element.column [ Element.width Element.fill, Element.centerY, Element.spacing 10 ]
+            , Element.column [ Element.width <| Element.maximum twoColMaximum Element.fill, Element.centerY, Element.spacing 10 ]
                 [ Element.el [ Element.centerX, Font.bold, Font.size 40 ] <| Element.text "Partage des capsules"
-                , Element.paragraph [ Element.width <| Element.maximum 400 Element.fill, Element.centerX ]
+                , Element.paragraph [ Element.centerX ]
                     [ Element.text "Aucune expertise technique n’est requise, pas de montage, pas de compression, pas de manipulations numériques des vidéos. Les capsules sont automatiquement stockées et publiées en ligne sur un serveur vidéo. Elles sont accessibles par un lien web (url) partageable par mail ou texto, sur les réseaux sociaux ou sur toute plateforme de formation (moodle, 360, etc.). La figure de gauche montre des vidéos polymny partagées sur MOODLE." ]
                 ]
             ]
-        , Element.row [ Element.width Element.fill, Element.height Element.fill, Element.padding 50, Element.spacing 20 ]
-            [ Element.column [ Element.width Element.fill, Element.centerY, Element.spacing 10 ]
+        , builder
+            [ Element.column [ Element.width <| Element.maximum twoColMaximum Element.fill, Element.centerY, Element.spacing 10 ]
                 [ Element.el [ Element.centerX, Font.bold, Font.size 40 ] <| Element.text "Enregistrement facile"
-                , Element.paragraph [ Element.width <| Element.maximum 400 Element.fill, Element.centerX ]
+                , Element.paragraph [ Element.centerX ]
                     [ Element.text "A la différence des logiciels d’enregistrement d’écrans, Polymny guide et facilite l’enregistrement des commentaires vidéos (avec la webcam) ou audios (avec le micro de votre ordinateur).  L’enregistrement se fait simplement depuis un navigateur web." ]
                 ]
             , Element.column
@@ -541,37 +560,37 @@ featuresView =
                 , Element.spacing 10
                 ]
                 [ Element.el [ Element.centerX, Border.rounded 100 ] <|
-                    Element.image [ Element.width <| Element.px 700 ]
+                    Element.image [ Element.width <| Element.px imageSize ]
                         { src = "/dist/recording.png"
                         , description = "S'enregsitrer"
                         }
                 ]
             ]
-        , Element.row [ Element.width Element.fill, Element.height Element.fill, Element.padding 50, Element.spacing 20 ]
+        , builder
             [ Element.column
                 [ Element.width Element.fill
                 , Element.centerY
                 , Element.spacing 10
                 ]
                 [ Element.el [ Element.centerX, Border.rounded 100 ] <|
-                    Element.image [ Element.width <| Element.px 700 ]
+                    Element.image [ Element.width <| Element.px imageSize ]
                         { src = "/dist/bigPicture.png"
                         , description = "Polymny en 4 étapes"
                         }
                 ]
-            , Element.column [ Element.width Element.fill, Element.centerY, Element.spacing 10 ]
+            , Element.column [ Element.width <| Element.maximum twoColMaximum Element.fill, Element.centerY, Element.spacing 10 ]
                 [ Element.el [ Element.centerX, Font.bold, Font.size 40 ] <| Element.text "4 étapes"
-                , Element.paragraph [ Element.width <| Element.maximum 400 Element.fill, Element.centerX ]
+                , Element.paragraph [ Element.centerX ]
                     [ Element.text "Le protocole de Polymny repose sur 4 étapes : préparer (en utilisant ou pas le prompteur), filmer (en se limitant éventuellement au son), produire (en plaçant les médias selon différents motifs) et publier (en obtenant le lien à partager sans aucun effort technique)." ]
                 ]
             ]
-        , Element.row [ Element.width Element.fill, Element.height Element.fill, Element.padding 50, Element.spacing 20 ]
-            [ Element.column [ Element.width Element.fill, Element.centerY, Element.spacing 10 ]
+        , builder
+            [ Element.column [ Element.width <| Element.maximum twoColMaximum Element.fill, Element.centerY, Element.spacing 10 ]
                 [ Element.el [ Element.centerX, Font.bold, Font.size 40 ] <| Element.text "Gérer les caspules"
-                , Element.paragraph [ Element.width <| Element.maximum 500 Element.fill, Element.centerX ]
+                , Element.paragraph [ Element.centerX ]
                     [ Element.text "Aucune minute passée pour s’enregistrer n’est perdue, chaque effort est modifiable et réutilisable plus tard : comme au cinéma, vous pouvez multiplier les «\u{00A0}prises\u{00A0}» pour satisfaire le réalisateur (vous\u{00A0}!).  Vous n’enregistrez plus votre écran mais vous gérez vos projets de capsules vidéos, d’une année sur l’autre, d’une classe à l’autre, d’une version initiale à l’amélioration suivante\u{00A0}! L’interface de Polymny ci-contre montre 3 projets d’un utilisateur\u{00A0}: 3 séquences pédagogiques comportant chacune plusieurs capsules vidéos, dont certaines en cours de préparation ne sont pas encore finalisées."
                     ]
-                , Element.paragraph [ Element.width <| Element.maximum 500 Element.fill, Element.centerX ]
+                , Element.paragraph [ Element.centerX ]
                     [ Element.text "Gérer chaque capsule comme un projet permet l’intervention d’ingénieurs pédagogiques ou de communicants dans la phase de préparation des ressources pédagogiques\u{00A0}: alignement pédagogique, ajustement de la durée du message ou de la clarté de la vidéo. "
                     ]
                 ]
@@ -581,31 +600,31 @@ featuresView =
                 , Element.spacing 10
                 ]
                 [ Element.el [ Element.centerX, Border.rounded 100 ] <|
-                    Element.image [ Element.width <| Element.px 700 ]
+                    Element.image [ Element.width <| Element.px imageSize ]
                         { src = "/dist/projectManagement.png"
                         , description = "Gestion de capsules"
                         }
                 ]
             ]
-        , Element.row [ Element.width Element.fill, Element.height Element.fill, Element.padding 50, Element.spacing 20 ]
+        , builder
             [ Element.column
                 [ Element.width Element.fill
                 , Element.centerY
                 , Element.spacing 10
                 ]
                 [ Element.el [ Element.centerX, Border.rounded 100 ] <|
-                    Element.image [ Element.width <| Element.px 700 ]
+                    Element.image [ Element.width <| Element.px imageSize ]
                         { src = "/dist/addResource.png"
                         , description = "ajout de vidéos additionelles"
                         }
                 ]
-            , Element.column [ Element.width Element.fill, Element.centerY, Element.spacing 10 ]
+            , Element.column [ Element.width <| Element.maximum twoColMaximum Element.fill, Element.centerY, Element.spacing 10 ]
                 [ Element.el [ Element.centerX, Font.bold, Font.size 40 ] <| Element.text "Vidéos additionelles"
-                , Element.paragraph [ Element.width <| Element.maximum 500 Element.fill, Element.centerX ]
+                , Element.paragraph [ Element.centerX ]
                     [ Element.text "Parmi les fonctionnalités plébiscitées, Polymny permet d’insérer, en lieu et place d’une diapositive, toute vidéo externe (captures de vos écrits sur tablettes pour les enseignants, vidéos ou screencasts issus d’un smartphone, clips libres de droit importés depuis internet). L’illustration suivante montre un jingle vidéo (une animation) insérée en guise de générique d’une vidéo de formation. Polymny est utilisé avec succès par des formateurs des sphères publiques (universités, lycées, MOOC FUN) et privées (organismes de formation professionnelle).\n" ]
                 ]
             ]
-        , Element.row [ Element.width Element.fill, Element.height Element.fill, Element.padding 50, Element.spacing 20 ]
+        , builder
             [ Element.column
                 [ Element.width Element.fill
                 , Element.centerY
@@ -613,32 +632,35 @@ featuresView =
                 ]
                 [ Element.el [ Element.centerX ] Element.none
                 , Element.paragraph [ Font.bold, Font.size 40, Font.center ] [ Element.text "Fonctions avancées" ]
-                , Element.textColumn [ Element.centerX, Element.paddingXY 20 2, Element.spacingXY 2 10 ]
-                    [ Element.paragraph [ Element.width <| Element.maximum 500 Element.fill, Element.centerX ]
+                , Element.textColumn [ Element.width <| Element.maximum twoColMaximum Element.fill, Element.centerX, Element.paddingXY 20 2, Element.spacingXY 2 10 ]
+                    [ Element.paragraph [ Element.centerX ]
                         [ Element.text "Des fonctionnalités avancées sont disponibles sous forme de services additionnels et optionnels."
                         ]
                     , Element.paragraph
-                        [ Element.width <| Element.maximum 500 Element.fill, Element.centerX ]
+                        [ Element.width <| Element.maximum twoColMaximum Element.fill, Element.centerX ]
                         [ Element.text "Si votre capsule, déjà préparée et prototypée sur le web, mérite une version professionnelle, l’équipe de Polymny met à votre disposition\u{00A0}:"
                         ]
-                    , Element.el [ Element.alignLeft, Element.padding 0 ] Element.none
-                    , Element.paragraph [ Element.alignLeft, Element.spacingXY 10 2 ]
+                    , Element.wrappedRow [ Element.alignLeft ]
                         [ Element.el
-                            [ Element.alignLeft
-                            , Element.paddingXY 20 50
+                            [ Element.alignTop
+                            , Element.paddingXY 10 0
                             , Font.bold
                             ]
-                            (Element.text "-")
-                        , Element.text "des services de production en studios audiovisuels virtuels (incrustations, keying/matting par deeplearning, génération graphique de background et foreground dynamiques, pointeurs, templates d’édition personnalisés, système de recommandation pour la grammaire du montage),"
+                            (Element.text "•")
+                        , Element.paragraph []
+                            [ Element.text <| "des services de production en studios audiovisuels virtuels (incrustations, keying/matting par deeplearning, génération graphique de background et foreground dynamiques, pointeurs, templates d’édition personnalisés, système de recommandation pour la grammaire du montage),"
+                            ]
                         ]
-                    , Element.paragraph [ Element.alignLeft, Element.spacingXY 10 2 ]
+                    , Element.wrappedRow [ Element.alignLeft ]
                         [ Element.el
-                            [ Element.alignLeft
-                            , Element.paddingXY 20 40
+                            [ Element.alignTop
+                            , Element.paddingXY 10 0
                             , Font.bold
                             ]
-                            (Element.text "-")
-                        , Element.text "des services de production en studios physiques accessibles par réservation (incrustations et keying fond vert, caméras et plans multiples, son haute qualité, scénarios de dialogues multi-micros, pré/postproduction). "
+                            (Element.text "•")
+                        , Element.paragraph []
+                            [ Element.text "des services de production en studios physiques accessibles par réservation (incrustations et keying fond vert, caméras et plans multiples, son haute qualité, scénarios de dialogues multi-micros, pré/postproduction). "
+                            ]
                         ]
                     ]
                 ]
@@ -648,7 +670,7 @@ featuresView =
                 , Element.spacing 10
                 ]
                 [ Element.el [ Element.centerX, Border.rounded 100 ] <|
-                    Element.image [ Element.width <| Element.px 500 ]
+                    Element.image [ Element.width <| Element.px imageSize ]
                         { src = "/dist/studioFondVert.png"
                         , description = "Studio fond vert"
                         }
