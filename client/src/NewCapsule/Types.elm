39becef89da1b67e232f2765070c33b5e3f5c440
Nicolas Bertrand <nico@inattendu.org>
2022-03-04 10:53:46 +0100

    Cleaner

--- client/src/NewCapsule/Types.elm
+++ client/src/NewCapsule/Types.elm
@@ -57,9 +57,6 @@ toggle split delimiter input =
 toggleAux : List Slide -> Bool -> Int -> List Slide -> List Slide
 toggleAux acc split delimiter input =
     case input of
-        [] ->
-            acc
-
         ( i1, g1, s1 ) :: ( i2, g2, s2 ) :: t ->
             if delimiter == -1 then
                 toggleAux (( i1, g1 + Utils.tern split 1 -1, s1 ) :: acc) split delimiter (( i2, g2, s2 ) :: t)
@@ -73,6 +70,9 @@ toggleAux acc split delimiter input =
 
             else
                 ( i1, g1, s1 ) :: acc
+
+        [] ->
+            acc
 
 
 {-| The message type for the new capsule page.
