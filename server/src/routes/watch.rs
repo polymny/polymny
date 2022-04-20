5726aaa5a545d416f6c5bbd2cdd1a84a035823ca
Thomas Forgione <thomas@forgione.fr>
2022-04-20 16:51:42 +0200

    Support for premium host and other host

--- server/src/routes/watch.rs
+++ server/src/routes/watch.rs
@@ -5,18 +5,26 @@ use std::path::PathBuf;
 use rocket::fs::NamedFile;
 use rocket::http::Status;
 use rocket::response::content::Html;
+use rocket::response::Redirect;
 use rocket::State as S;
 
 use crate::config::Config;
 use crate::db::capsule::{Capsule, Privacy, Role};
 use crate::db::task_status::TaskStatus;
+use crate::db::user::Plan;
 use crate::db::user::User;
+use crate::routes::Either;
 use crate::templates::video_html;
 use crate::{Db, Error, HashId, Result};
 
 /// The route that serves HTML to watch videos.
 #[get("/v/<capsule_id>", rank = 1)]
-pub async fn watch<'a>(user: Option<User>, capsule_id: HashId, db: Db) -> Result<Html<String>> {
+pub async fn watch<'a>(
+    config: &S<Config>,
+    user: Option<User>,
+    capsule_id: HashId,
+    db: Db,
+) -> Result<Either<Html<String>, Redirect>> {
     let capsule = Capsule::get_by_id(*capsule_id as i32, &db)
         .await?
         .ok_or(Error(Status::NotFound))?;
@@ -36,10 +44,30 @@ pub async fn watch<'a>(user: Option<User>, capsule_id: HashId, db: Db) -> Result
         }
     }
 
-    Ok(Html(video_html(&format!(
+    // Check if video is on current host or other host.
+
+    // If there is another host
+    if let Some(host) = &config.other_host {
+        // Look of the owner of the capsule
+        for (user, role) in capsule.users(&db).await? {
+            if role == Role::Owner {
+                // If premium state doesn't match the owner plan
+                if config.premium_only != (user.plan >= Plan::PremiumLvl1) {
+                    // Redirect to the other host
+                    return Ok(Either::Right(Redirect::to(format!(
+                        "{}/v/{}/",
+                        host,
+                        capsule_id.hash()
+                    ))));
+                }
+            }
+        }
+    }
+
+    Ok(Either::Left(Html(video_html(&format!(
         "/v/{}/manifest.m3u8",
         capsule_id.hash()
-    ))))
+    )))))
 }
 
 /// The route that serves files inside published videos.
