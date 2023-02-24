ec1c3abe6eaef3b11916ad9b8889381c3f4906f7
gdamms <damguillotin@gmail.com>
2023-02-24 13:59:17 +0100

    fix: forgot subs

--- client/src/Preparation/Updates.elm
+++ client/src/Preparation/Updates.elm
@@ -14,6 +14,7 @@ import Data.User as Data
 import Dict exposing (Dict)
 import File
 import File.Select as Select
+import Keyboard
 import List.Extra
 import Preparation.Types as Preparation
 import RemoteData
@@ -287,13 +288,28 @@ fixStructure old new =
     ( broken, ret )
 
 
+{-| Keyboard shortcuts of the preparation page.
+-}
+shortcuts : Keyboard.RawKey -> App.Msg
+shortcuts msg =
+    case Keyboard.rawValue msg of
+        "Escape" ->
+            App.PreparationMsg <| Preparation.EscapePressed
+
+        _ ->
+            App.Noop
+
+
 {-| Subscriptions for the prepration view.
 -}
 subs : Preparation.Model -> Sub App.Msg
 subs model =
     Sub.batch
-        [ Preparation.slideSystem.subscriptions model.slideModel
-        , Preparation.gosSystem.subscriptions model.gosModel
+        [ Sub.batch
+            [ Preparation.slideSystem.subscriptions model.slideModel
+            , Preparation.gosSystem.subscriptions model.gosModel
+            ]
+            |> Sub.map Preparation.DnD
+            |> Sub.map App.PreparationMsg
+        , Keyboard.ups shortcuts
         ]
-        |> Sub.map Preparation.DnD
-        |> Sub.map App.PreparationMsg
